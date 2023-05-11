a82d05191646096262c90a18060543795b670c6b
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-05-11 15:44:27 +0200

    feat: get data weather app

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,32 @@
-import Image from 'next/image';
+'use client';
+
+import { useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+   const [city, setCity] = useState('');
+   const [weather, setWeather] = useState({});
+   const [loading, setLoading] = useState(false);
+
    const url = `https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
+   const fetchWeather = (e) => {
+      e.preventDefault();
+      setLoading(true);
+
+      axios.get(url).then((response) => {
+         console.log('data', response.data);
+         setWeather(response.data);
+      });
+
+      setCity('');
+      setLoading(false);
+   };
+
    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-            <Image
-               className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-               src="/next.svg"
-               alt="Next.js Logo"
-               width={180}
-               height={37}
-               priority
-            />
+      <main>
+         <div>
+            <h1 className="text-2xl font-semibold">App Weather</h1>
          </div>
       </main>
    );
