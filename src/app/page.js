627d2f3b2001add101309c5b6561bec9f0a11e68
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-05-11 18:59:25 +0200

    feat: create Weather component

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,23 @@
 'use client';
 
 import { useState } from 'react';
+import { BsSearch } from 'react-icons/bs';
 import axios from 'axios';
 import Image from 'next/image';
-import { BsSearch } from 'react-icons/bs';
+import Weather from '@/components/Weather';
 
 export default function Home() {
    const [city, setCity] = useState('');
    const [weather, setWeather] = useState({});
    const [loading, setLoading] = useState(false);
 
-   const url = `https://api.openweathermap.org/data/2.5/weather?q=barcelona&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
 
    const fetchWeather = (e) => {
       e.preventDefault();
       setLoading(true);
 
       axios.get(url).then((response) => {
-         console.log('data', response.data);
          setWeather(response.data);
       });
 
@@ -25,8 +25,14 @@ export default function Home() {
       setLoading(false);
    };
 
+   const handleSearch = (e) => {
+      if (e.target.value !== '') {
+         return setCity(e.target.value);
+      }
+   };
+
    return (
-      <div className="relative h-screen ">
+      <div className="relative h-screen">
          <div className="absolute inset-0">
             <Image
                src="https://images.unsplash.com/photo-1493243350443-9e3048ce7288?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80"
@@ -34,18 +40,30 @@ export default function Home() {
                fill
             />
          </div>
+
          {/* Búsqueda */}
-         <div className="relative z-10 m-auto flex w-full max-w-[500px] items-center justify-between  pt-4">
-            <form className="m-auto flex w-full items-center justify-between rounded-md bg-black/40 p-3">
-               <input
-                  type="text"
-                  placeholder="Buscar ciudad"
-                  className="h-8 w-full rounded-md border-none bg-transparent pl-3 text-2xl text-gray-200 placeholder:text-gray-200 focus:outline-none"
-               />
-               <button onClick={fetchWeather}>
-                  <BsSearch color="text-gray-200" size={25} />
-               </button>
-            </form>
+         <div className="flex h-screen flex-col justify-between">
+            <div className="relative z-10 m-auto flex max-w-[500px]">
+               <form
+                  onSubmit={fetchWeather}
+                  className="m-auto flex w-full items-center justify-between rounded-md bg-black/40 p-3"
+               >
+                  <input
+                     type="text"
+                     placeholder="Buscar ciudad"
+                     className="h-8 w-full rounded-md border-none bg-transparent pl-3 text-2xl text-gray-200 placeholder:text-gray-200 focus:outline-none"
+                     onChange={handleSearch}
+                  />
+                  <button onClick={fetchWeather}>
+                     <BsSearch color="rgb(229 231 235)" size={25} />
+                  </button>
+               </form>
+            </div>
+
+            {/* Clima (dashboard) */}
+            <div className="relative z-10 m-auto flex max-w-[800px]">{weather?.main && <Weather data={weather} />}</div>
+
+            {/* Otro */}
          </div>
       </div>
    );
---
+++ src/components/Weather.jsx
@@ -0,0 +1,8 @@
+export default function Weather({ data }) {
+   console.log('data:', data.name);
+   return (
+      <div className="m-auto flex w-full items-center justify-between rounded-md bg-black/40 p-3">
+         <h1>{data.name}</h1>
+      </div>
+   );
+}
