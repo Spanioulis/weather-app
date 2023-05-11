d867b05cd5eac6bcca1eb2570a47fb5fad2531e5
Sergio Paniagua Sánchez <97700906+Spanioulis@users.noreply.github.com>
2023-05-11 19:47:09 +0200

    feat: add open weather icon to dashboard

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,8 +1,16 @@
+import Image from 'next/image';
+
 export function Weather({ data }) {
-   console.log('data:', data.name);
+   console.log('data:', data);
    return (
       <div className="m-auto flex w-full items-center justify-between rounded-md bg-black/40 p-3">
          <h1>{data.name}</h1>
+         <Image
+            src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+            alt="/"
+            width="100px"
+            height="100px"
+         />
       </div>
    );
 }
